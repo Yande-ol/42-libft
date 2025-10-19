0245b1f018b4e059b21b03c2c9fd7f4f3fdbb5cf
Yande-ol <yaancarlos8@icloud.com>
2025-10-19 14:54:59 +0100

    Refactor function: ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: Yande-ol <Yande-ol@student.42.fr>          #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-10-05 21:12:16 by Yande-ol          #+#    #+#             */
-/*   Updated: 2025-10-05 21:12:16 by Yande-ol         ###   ########.fr       */
+/*   Created: 2025-10-18 14:13:48 by Yande-ol          #+#    #+#             */
+/*   Updated: 2025-10-18 14:13:48 by Yande-ol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char	*ptr;
+	size_t			i;
+	unsigned char	*ptr;
 
-	ptr = (const unsigned char *)s;
-	while (n--)
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
 	{
-		if (*ptr == (unsigned char)c)
-			return ((void *)(ptr));
-		ptr++;
+		if (ptr[i] == (unsigned char)c)
+			return ((void *)&ptr[i]);
+		i++;
 	}
 	return (NULL);
 }
